b28a4abe9db2a9fb14d53b3a442484b65fa1625c
Stjepan Ruklić <sr51641@fer.hr>
2021-08-11 16:26:01 +0200

    Added spring data jpa service for Pet. Closes #43.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/jpa/PetJPAService.java
@@ -0,0 +1,47 @@
+package guru.springframework.sfgpetclinic.services.jpa;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.repositories.PetRepository;
+import guru.springframework.sfgpetclinic.services.PetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PetJPAService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetJPAService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+
+        Set<Pet> optionalPets = new HashSet<>();
+
+        petRepository.findAll().iterator().forEachRemaining(optionalPets::add);
+
+        return optionalPets;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
