c95c1335e6c618755d2ee099da1f0c9558276a1f
Stjepan Ruklić <sr51641@fer.hr>
2021-08-11 16:14:41 +0200

    Added spring data jpa service for owners. Closes #40.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetRepository.java
@@ -1,7 +1,6 @@
 package guru.springframework.sfgpetclinic.repositories;
 
 import guru.springframework.sfgpetclinic.model.Pet;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PetRepository extends CrudRepository<Pet, Long> {
